13cbbda8753f91f119f9ed453451b475846c96fe
Mojtaba <mojtabaahmadi40@gmail.com>
2015-10-21 00:47:45 +0330

    Add "BasePage" class

---
+++ src/App_Code/BasePage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Data.SqlClient;
+
+public class BasePage : System.Web.UI.Page
+{
+    public string ConnectionString = ConfigurationManager.ConnectionStrings["cnn"].ConnectionString;
+
+    public DataTable doQuery(string sql)
+    {
+        DataTable table = new DataTable();
+        SqlDataAdapter adpr;
+        SqlConnection cnn;
+        SqlCommand cmd;
+
+        cnn = new SqlConnection(this.ConnectionString);
+        cmd = new SqlCommand(sql, cnn);
+
+        using (adpr = new SqlDataAdapter(cmd))
+        {
+            adpr.Fill(table);
+        }
+
+        return table;
+    }
+}
