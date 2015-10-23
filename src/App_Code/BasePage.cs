77a4b5287f3b99abe9dd8b92098a0859b3b9ba42
Mimalf <mojtabaahmadi40@gmail.com>
2015-10-23 22:37:28 +0000

    Add some common methods

--- src/App_Code/BasePage.cs
+++ src/App_Code/BasePage.cs
@@ -13,23 +13,100 @@ using System.Data.SqlClient;
 
 public class BasePage : System.Web.UI.Page
 {
-    public string ConnectionString = ConfigurationManager.ConnectionStrings["cnn"].ConnectionString;
+    private string connectionString = ConfigurationManager.ConnectionStrings["cnn"].ConnectionString;
+    private string currentUrl = HttpContext.Current.Request.Url.AbsolutePath;
 
-    public DataTable doQuery(string sql)
+    private void sqlError()
+    {
+        Response.Cookies["error_message"].Value = "خطای پایگاه داده.";
+        Response.Cookies["error_redirect"].Value = this.currentUrl;
+        Response.Redirect("PublicError.aspx");
+    }
+
+    public void insertQuery(string sql)
+    {
+        SqlConnection cnn;
+        SqlCommand cmd;
+
+        cnn = new SqlConnection(this.connectionString);
+
+        try
+        {    
+            cnn.Open();
+
+            cmd = new SqlCommand(sql, cnn);
+            cmd.ExecuteNonQuery();
+        }
+        catch
+        {
+            sqlError();
+        }
+        finally
+        {
+            cnn.Close();
+        }
+    }
+
+    public DataTable selectQuery(string sql)
     {
         DataTable table = new DataTable();
         SqlDataAdapter adpr;
         SqlConnection cnn;
         SqlCommand cmd;
 
-        cnn = new SqlConnection(this.ConnectionString);
-        cmd = new SqlCommand(sql, cnn);
+        cnn = new SqlConnection(this.connectionString);
 
-        using (adpr = new SqlDataAdapter(cmd))
+        try
         {
+            cnn.Open();
+
+            cmd = new SqlCommand(sql, cnn);
+            adpr = new SqlDataAdapter(cmd);
             adpr.Fill(table);
+        }
+        catch
+        {
+            sqlError();
+        }
+        finally
+        {
+            cnn.Close();
         }
 
         return table;
+    }
+
+    public void fillTable(string sql, ref Table table)
+    {
+        DataTable dataTable = selectQuery(sql);
+        
+        foreach (DataRow row in dataTable.Rows)
+        {
+            TableRow trow = new TableRow();
+
+            for (int i = 0; i < dataTable.Columns.Count; i++)
+            {
+                TableCell cell = new TableCell();
+
+                cell.Text = row[i].ToString();
+
+                trow.Cells.Add(cell);
+            }
+
+            if (table.ID == "TableEquip")
+            {
+                TableCell cell = new TableCell();
+                LinkButton edit = new LinkButton();
+                
+                edit.PostBackUrl = "~/PersonnelEditEquip.aspx?equip=";
+                edit.PostBackUrl += row["id"].ToString();
+                edit.Text = "ویرایش";
+
+                cell.Controls.Add(edit);
+                trow.Cells.Add(cell);
+            }
+
+            table.Rows.Add(trow);
+        }
     }
 }
